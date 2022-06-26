e81b1be9adaa38ace1c7e388682194f61aeffe2b
AyrielNoriega <anoriega2310@gmail.com>
2022-06-25 19:14:53 -0500

    config de vistas

--- views/home.php
+++ views/home.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="col-8">
+    <h1>Página Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque minima, aperiam ut maiores maxime corporis! Voluptatibus tempora officiis nesciunt vel officia, hic expedita quasi architecto odit repellendus, perspiciatis corporis rerum.</p>
+</div>
